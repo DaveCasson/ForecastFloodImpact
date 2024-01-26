86b86c34b2297f99b7dc40e4992a8c7628bed3e5
DaveCasson <davecasson@gmail.com>
2024-01-26 09:41:24 -0500

    Update to scalar data access

--- scripts/hydrograph_plotting.py
+++ scripts/hydrograph_plotting.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import timedelta
 
-def convert_to_daily_mean(realtime_df):
+def convert_to_daily_mean(realtime_df, variable, date_col='DATETIME'):
     """
     Convert the realtime_df DataFrame to daily mean values for the "LEVEL" and "DISCHARGE" columns.
 
@@ -14,17 +14,17 @@ def convert_to_daily_mean(realtime_df):
     DataFrame: Pandas DataFrame with daily mean values for the "LEVEL" and "DISCHARGE" columns.
     """
     # Convert the "DATETIME" column to datetime type
-    realtime_df['DATETIME'] = pd.to_datetime(realtime_df['DATETIME'])
+    realtime_df[date_col] = pd.to_datetime(realtime_df[date_col])
 
     # Set the "DATETIME" column as the index
-    realtime_df.set_index('DATETIME', inplace=True)
+    realtime_df.set_index(date_col, inplace=True)
 
     # Resample the DataFrame to daily frequency and calculate the mean for "LEVEL" and "DISCHARGE" columns
-    daily_mean_df = realtime_df['DISCHARGE'].resample('D').mean()
+    daily_mean_df = realtime_df[variable].resample('D').mean()
 
     return daily_mean_df
 
-def calculate_daily_percentiles_of_historic_data(df):
+def calculate_daily_percentiles_of_historic_data(df, variable):
     """
     Calculate the daily percentiles for the discharge data.
     
@@ -35,72 +35,63 @@ def calculate_daily_percentiles_of_historic_data(df):
 
     tuple: Three DataFrames containing the max-min, 90-10 percentile, and 25-75 percentile ranges.
     """
+    df['DATE'] = pd.to_datetime(df['DATE'])
     df['MONTH_DAY'] = df['DATE'].dt.strftime('%m-%d')
+
+    historic_range_df = {}
     max_min_range = {}
-    percentile_90_10 = {}
-    percentile_25_75 = {}
+    percentile_range_1 = {}
+    percentile_range_2= {}
 
     for month_day in df['MONTH_DAY'].unique():
-        daily_values = df[df['MONTH_DAY'] == month_day]['DISCHARGE'].dropna()
+        daily_values = df[df['MONTH_DAY'] == month_day][variable].dropna()
         if len(daily_values) == 0:
             continue
         max_min_range[month_day] = [daily_values.max(), daily_values.min()]
-        percentile_90_10[month_day] = [np.percentile(daily_values, 90), np.percentile(daily_values, 10)]
-        percentile_25_75[month_day] = [np.percentile(daily_values, 75), np.percentile(daily_values, 25)]
+        percentile_range_1[month_day] = [np.percentile(daily_values, 90), np.percentile(daily_values, 10)]
+        percentile_range_2[month_day] = [np.percentile(daily_values, 75), np.percentile(daily_values, 25)]
 
-    max_min_df = pd.DataFrame.from_dict(max_min_range, orient='index', columns=['Max', 'Min'])
-    percentile_90_10_df = pd.DataFrame.from_dict(percentile_90_10, orient='index', columns=['90th', '10th'])
-    percentile_25_75_df = pd.DataFrame.from_dict(percentile_25_75, orient='index', columns=['75th', '25th'])
+    historic_range_df = pd.concat([pd.DataFrame.from_dict(max_min_range, orient='index', columns=['Max', 'Min']),
+                            pd.DataFrame.from_dict(percentile_range_1, orient='index', columns=['90th', '10th']),
+                            pd.DataFrame.from_dict(percentile_range_2, orient='index', columns=['75th', '25th'])], axis=1)
 
     # Sort the index in the desired order
     index_order = pd.Index(pd.date_range(start='2000-10-01', end='2001-09-30')).strftime('%m-%d')
-    max_min_df = max_min_df.reindex(index_order)
-    percentile_90_10_df = percentile_90_10_df.reindex(index_order)
-    percentile_25_75_df = percentile_25_75_df.reindex(index_order)
+    historic_range_df = historic_range_df.reindex(index_order)
 
-    return max_min_df, percentile_90_10_df, percentile_25_75_df
+    return historic_range_df
 
-def plot_annual_hydrograph_statistics(max_min_df, percentile_90_10_df, percentile_25_75_df, realtime_df_daily):
-    """
-    Plot the hydrograph with percentile ranges.
+def plot_annual_hydrograph_statistics(station_number,variable, historic_range_df, realtime_df_daily, save_png=False, png_path=None): 
 
-    Args:
-    max_min_df (DataFrame): DataFrame containing max-min range.
-    percentile_90_10_df (DataFrame): DataFrame containing 90-10 percentile range.
-    percentile_25_75_df (DataFrame): DataFrame containing 25-75 percentile range.
-    realtime_df_daily (DataFrame): DataFrame containing the daily mean values for the "DISCHARGE" column.
-    """
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))# sharex=True)
 
     # Plot the main hydrograph
-    ax1.fill_between(max_min_df.index, max_min_df['Max'], max_min_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
-    ax1.fill_between(percentile_90_10_df.index, percentile_90_10_df['90th'], percentile_90_10_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
-    ax1.fill_between(percentile_25_75_df.index, percentile_25_75_df['75th'], percentile_25_75_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
+    ax1.fill_between(historic_range_df.index, historic_range_df['Max'], historic_range_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
+    ax1.fill_between(historic_range_df.index, historic_range_df['90th'], historic_range_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
+    ax1.fill_between(historic_range_df.index, historic_range_df['75th'], historic_range_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
     ax1.plot(realtime_df_daily.index, realtime_df_daily.values, color='red', label='RealTime')
-    ax1.set_title('Hydrograph for Complete Water Year')
-    ax1.set_ylabel('Discharge')
+    ax1.set_title(f'{variable} Hydrograph for Complete Water Year, Station {station_number}')
+    ax1.set_ylabel(variable)
     ax1.legend()
     ax1.set_ylim(0,)
-    ax1.set_xlim(max_min_df.index[0], max_min_df.index[-1])
+    ax1.set_xlim(historic_range_df.index[0], historic_range_df.index[-1])
     ax1.grid(True)
 
     # Format the x-axis tick labels
-    ax1.set_xticks(np.arange(0, len(max_min_df), step=len(max_min_df)//12))
-    ax1.set_xticklabels([date[0:5] for date in max_min_df.index[::len(max_min_df)//12]], rotation=45)
+    ax1.set_xticks(np.arange(0, len(historic_range_df), step=len(historic_range_df)//12))
+    ax1.set_xticklabels([date[0:5] for date in historic_range_df.index[::len(historic_range_df)//12]], rotation=45)
 
     #Subset the data to the period of interest (defined by realtime_df_daily.index)
-    max_min_df = max_min_df.loc[realtime_df_daily.index]
-    percentile_90_10_df = percentile_90_10_df.loc[realtime_df_daily.index]
-    percentile_25_75_df = percentile_25_75_df.loc[realtime_df_daily.index]
-
+    historic_range_df = historic_range_df.loc[realtime_df_daily.index]
+    
     # Plot the zoomed-in hydrograph
-    ax2.fill_between(max_min_df.index, max_min_df['Max'], max_min_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
-    ax2.fill_between(percentile_90_10_df.index, percentile_90_10_df['90th'], percentile_90_10_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
-    ax2.fill_between(percentile_25_75_df.index, percentile_25_75_df['75th'], percentile_25_75_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
+    ax2.fill_between(historic_range_df.index, historic_range_df['Max'], historic_range_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
+    ax2.fill_between(historic_range_df.index, historic_range_df['90th'], historic_range_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
+    ax2.fill_between(historic_range_df.index, historic_range_df['75th'], historic_range_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
     ax2.plot(realtime_df_daily.index, realtime_df_daily.values, color='red', label='RealTime')
-    ax2.set_title('Hydrograph for Real-Time and Forecasted Data')
+    ax2.set_title(f'{variable} for Real-Time and Forecasted Data, Station {station_number}')
     ax2.set_xlabel('Day of the Year')
-    ax2.set_ylabel('Discharge')
+    ax2.set_ylabel(variable)
     ax2.legend()
     ax2.grid(True)
 
@@ -109,30 +100,31 @@ def plot_annual_hydrograph_statistics(max_min_df, percentile_90_10_df, percentil
     #Set lower y-axis limit to 0
     ax2.set_ylim(0,)
 
-    ax2.set_xticks(np.arange(0, len(max_min_df), step=len(max_min_df)//12))
-    ax2.set_xticklabels([date[0:5] for date in max_min_df.index[::len(max_min_df)//12]], rotation=45)
+    ax2.set_xticks(np.arange(0, len(historic_range_df), step=len(historic_range_df)//12))
+    ax2.set_xticklabels([date[0:5] for date in historic_range_df.index[::len(historic_range_df)//12]], rotation=45)
 
     plt.tight_layout()
     plt.show()
 
-if __name__ == '__main__':
-    df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/historic/05BL022_historic.csv')
-    realtime_df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/realtime/05BL022_realtime.csv')
+    if save_png:
+        plt.savefig(png_path, dpi=300)
 
-    realtime_df_daily = convert_to_daily_mean(realtime_df)
+def plot_realtime_hydrograph_with_historical_ranges(station_number,variable,historic_df, realtime_df, save_png=False, png_path=None):
+
+    historic_range_df = calculate_daily_percentiles_of_historic_data(historic_df,variable)
+
+    realtime_df_daily = convert_to_daily_mean(realtime_df,variable)
 
     month_day = realtime_df_daily.index.strftime('%m-%d')
-    #realtime_df_daily.set_index(month_day, inplace=True)
-
-    #Create a new dataframe with month_day as index
     daily_mean_df = pd.DataFrame(realtime_df_daily.values, index=month_day)
+    
+    plot_annual_hydrograph_statistics(station_number, variable, historic_range_df, daily_mean_df,save_png, png_path)
 
-            
+if __name__ == '__main__':
 
-    df['DATE'] = pd.to_datetime(df['DATE'])
-    max_min_df, percentile_90_10_df, percentile_25_75_df = calculate_daily_percentiles_of_historic_data(df)
-    plot_annual_hydrograph_statistics(max_min_df, percentile_90_10_df, percentile_25_75_df, daily_mean_df)
+    df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/historic/05BL022_historic.csv')
+    realtime_df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/realtime/05BL022_realtime.csv')
+    png_path = '/Users/drc858/GitHub/ForecastFloodImpact/output/05BL022_hydrograph.png'
+    variable = 'DISCHARGE'
 
-
-
-
+    plot_realtime_hydrograph_with_historical_ranges('05BL022',variable, df, realtime_df, save_png=True, png_path=png_path)
--- scripts/scalar_data_access.py
+++ scripts/scalar_data_access.py
@@ -1,145 +1,64 @@
-# Import needed modules
+# Description: This script contains functions to retrieve scalar data from the Environment and Climate Change Canada (ECCC) API.
 
-from datetime import date
 from owslib.ogcapi.features import Features
 import pandas as pd
-import yaml
-from pprint import pprint
-
 from pathlib import Path
-# Set up logger
 import logging
+import copy 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def retrieve_data_from_api(stations, collection, download_variable,datetime_column, api_url, output_dir,other_variables=[],time_limits=False, start_date=None, end_date=None, limit=10000):
 
-def retrieve_real_time_data(stations, start_date, end_date, api_url, output_dir, output_suffix='', limit=10000):
-    """
-    Retrieve real-time hydrometric data for a given time period and save it to CSV files.
-    
-    Parameters:
-    - stations (list): List of hydrometric station IDs.
-    - start_date (datetime.date): Start date of the time period.
-    - end_date (datetime.date): End date of the time period.
-    - output_dir (str or pathlib.Path): Directory path where the output CSV files will be saved.
-    - output_suffix (str, optional): Suffix to be added to the output CSV file names. Default is an empty string.
-    - limit (int, optional): Maximum number of data points to retrieve per station. Default is 10000.
-    
-    Returns:
-    - None
-    
-    Raises:
-    - ValueError: If no water level data is returned for any station.
-    """
-    
-    time_ = f"{start_date}/{end_date}"
-    logger.info(f"Retrieving hydrometric data for the period {time_}")
-
-    # Instansiate features
-    oafeat = Features(api_url)
-    
-    # List of stations with no water level data
-    stations_without_data = []
-
-    # Data retrieval and creation of the data frames
-    for station in stations:
-
-        # Retrieval of water level data
-        hydro_data = oafeat.collection_items(
-            "hydrometric-realtime",
-            limit=limit,
-            datetime=time_,
-            STATION_NUMBER=station,
-        )
-
-        # Creation of a data frame if there is data for the chosen time period
-        if hydro_data["features"]:
-            
-            # Creation of a dictionary in a format compatible with Pandas
-            historical_data_format = [
-                {
-                    # Note coordinates can be pulled as below, but they are not currently used
-                    #"LATITUDE": el["geometry"]["coordinates"][1],
-                    #"LONGITUDE": el["geometry"]["coordinates"][0],
-                    **el["properties"],
-                }
-                for el in hydro_data["features"]
-            ]
-            # Creation of the data frame
-            historical_data_df = pd.DataFrame(
-                historical_data_format,
-                columns=[
-                    "STATION_NUMBER",
-                    "STATION_NAME",
-                    "DATETIME",
-                    "LEVEL",
-                    "DISCHARGE"
-                ],
-            )
-            
-            # Detect and convert data types of columns
-            historical_data_df = historical_data_df.infer_objects(copy=False)
-
-            # Creating an index with the date in a datetime format
-            historical_data_df["DATETIME"] = pd.to_datetime(
-                historical_data_df["DATETIME"]
-            )
-            historical_data_df.set_index(["DATETIME"], inplace=True, drop=True)
-            
-            # Output data frame to csv file
-            output_csv_path = Path(output_dir,f'{station}{output_suffix}.csv')
-            historical_data_df.to_csv(output_csv_path, index=True)
-
-            logger.info(f"Data output to {output_csv_path}")
-        
-        # If there is no data for the chosen time period, the station
-        # will be removed from the dataset
-        else:
-            stations_without_data.append(station)
-
-    # Removing hydrometric stations without water level data from the station list
-    for station in stations_without_data:
-        logger.warning(
-            f"Station {station} has no water level data for the chosen time period."
-        )
-        stations.remove(station)
-
-    # Raising an error if no station is left in the list
-    if not stations:
-        raise ValueError(
-            f"No water level data was returned, please check the query."
-        )
-    
-def retrieve_historic_data(stations, collection, variables, start_date, end_date,  output_dir, output_suffix, api_url="https://api.weather.gc.ca/", limit=5000):
-
-    time_ = f"{start_date}/{end_date}"
-    logger.info(f"Retrieving hydrometric data for the period {time_}")
-
-    # Instansiate features
-    oafeat = Features(api_url)
+    # Set the time limits for the data retrieval
+    if time_limits:
+        time_ = f"{start_date}/{end_date}"
+        logger.info(f"Retrieving {download_variable} from {collection} for the period {time_}")
+    else:
+        logger.info(f"Retrieving {download_variable} from {collection} with no time limits")
 
     # Set columns to be saved to the dataframe
-    base_columns = [
+    query_variables = [
                     "STATION_NUMBER",
-                    "STATION_NAME",
-                    "DATE",
+                    "STATION_NAME"
                     ]
-    df_columns = base_columns + variables
-    print(df_columns)
+    
+    # Append additional query variables
+    query_variables.append(datetime_column)
+    query_variables.append(download_variable)
+    query_variables = query_variables + other_variables
+    logger.info(f"Query variables: {query_variables}")
 
+    # Output data frame to csv file
+    collection_output_dir = Path(output_dir,collection)
+    collection_output_dir.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Output directory: {collection_output_dir}")
+
+    # Instansiate features
+    oafeat = Features(api_url)
+    
     # List of stations with no water level data
+    stations_with_data = copy.copy(stations)
     stations_without_data = []
 
     # Data retrieval and creation of the data frames
     for station in stations:
-
-        # Retrieval of water level data
-        hydro_data = oafeat.collection_items(
-            collection,
-            #datetime=time_,
-            limit=limit,
-            STATION_NUMBER=station,
-        )
+        if time_limits:
+            # Retrieval of water level data
+            hydro_data = oafeat.collection_items(
+                collection,
+                limit=limit,
+                STATION_NUMBER=station,
+                time=time_,
+            )
+        else:
+            # Retrieval of water level data
+            hydro_data = oafeat.collection_items(
+                collection,
+                limit=limit,
+                STATION_NUMBER=station,
+            )
 
         # Creation of a data frame if there is data for the chosen time period
         if hydro_data["features"]:
@@ -151,68 +70,45 @@ def retrieve_historic_data(stations, collection, variables, start_date, end_date
                 }
                 for el in hydro_data["features"]
             ]
+
             # Creation of the data frame
             historical_data_df = pd.DataFrame(
                 historical_data_format,
-                columns=df_columns
+                columns=query_variables,
             )
             
             # Detect and convert data types of columns
             historical_data_df = historical_data_df.infer_objects(copy=False)
 
             # Creating an index with the date in a datetime format
-            historical_data_df["DATE"] = pd.to_datetime(
-                historical_data_df["DATE"]
+            historical_data_df[datetime_column] = pd.to_datetime(
+                historical_data_df[datetime_column]
             )
-            historical_data_df.set_index(["DATE"], inplace=True, drop=True)
+            historical_data_df.set_index([datetime_column], inplace=True, drop=True)
             
-            # Output data frame to csv file
-            output_csv_path = Path(output_dir,f'{station}{output_suffix}.csv')
+            output_csv_path = Path(collection_output_dir,f'{station}_{download_variable}.csv')
             historical_data_df.to_csv(output_csv_path, index=True)
 
-            logger.info(f"Data output to {output_csv_path}")
+            logger.info(f"{download_variable} from {collection} for station {station} output to {output_csv_path}")
         
         # If there is no data for the chosen time period, the station
         # will be removed from the dataset
         else:
             stations_without_data.append(station)
 
+
     # Removing hydrometric stations without water level data from the station list
     for station in stations_without_data:
         logger.warning(
-            f"Station {station} has no water level data for the chosen time period."
+            f"Station {station} has no {download_variable} data for the chosen time period."
         )
-        stations.remove(station)
+        stations_with_data.remove(station)
 
     # Raising an error if no station is left in the list
     if not stations:
+        stations_with_data = []
         raise ValueError(
-            f"No water level data was returned, please check the query."
+            f"No {download_variable} data was returned from {collection}, please check the query."
         )
-
-if __name__ == "__main__":
-    # Read yaml file
-    config_file = "../settings/bow_watershed_settings.yaml"
-    with open(config_file, "r") as f:
-        config = yaml.safe_load(f)
-    logger.info(f"Config file: {pprint(config)}")
-
-    # Read in the list of hydrometric stations
-    hydro_stations_df = pd.read_csv(config["hydro_stations_csv"])
-    search_stations = hydro_stations_df["ID"].tolist()
-    logger.info(f"Hydrometric stations: {search_stations}")   
-
-    # Create an instance of the Features class
-    oafeat = Features("https://api.weather.gc.ca/")
-
-    # Start and end of the time period for which the data will be retrieved
-    end_date = date.today()
-    years_to_search = 100
-    days_to_search = years_to_search * 365
-
-    start_date = end_date - pd.Timedelta(days=days_to_search)
-
-    output_dir = Path('../output/historic')
-    output_suffix = '_historic'
-
-    retrieve_historic_data(search_stations, start_date, end_date, output_dir, output_suffix,limit=days_to_search)
+    
+    return stations_with_data
