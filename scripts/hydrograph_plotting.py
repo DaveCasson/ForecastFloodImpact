1f82f5e92706d4e449a377aaecf2b32944a81927
DaveCasson <davecasson@gmail.com>
2024-01-29 16:56:50 -0500

    Update with basic forecast and thresholds workng

--- scripts/hydrograph_plotting.py
+++ scripts/hydrograph_plotting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import timedelta
+from pathlib import Path
 
 def convert_to_daily_mean(realtime_df, variable, date_col='DATETIME'):
     """
@@ -24,6 +24,25 @@ def convert_to_daily_mean(realtime_df, variable, date_col='DATETIME'):
 
     return daily_mean_df
 
+def add_thresholds(threshold_df, ax):
+
+    # Define different line styles
+    # Define more line styles to ensure uniqueness
+    line_styles = ['--', '-.', ':', (0, (3, 5, 1, 5)), '-',(0, (5, 10)), (0, (1, 10)), (0, (5, 1)), 
+                (0, (3, 1, 1, 1)), (0, (3, 10, 1, 10))]
+
+    # Adjust alpha for less pronounced lines
+    alpha_value = 0.6
+    
+    # Cycle through the line styles for different return periods
+    for i, row in threshold_df.iterrows():
+        t_year = row['T-yrs']
+        value = row[threshold_df.columns[-1]]
+        line_style = line_styles[i % len(line_styles)]
+        ax.axhline(y=value, color='red', linestyle=line_style,alpha=alpha_value, label=f'T-{t_year} years')
+
+    return ax
+    
 def calculate_daily_percentiles_of_historic_data(df, variable):
     """
     Calculate the daily percentiles for the discharge data.
@@ -56,75 +75,113 @@ def calculate_daily_percentiles_of_historic_data(df, variable):
                             pd.DataFrame.from_dict(percentile_range_2, orient='index', columns=['75th', '25th'])], axis=1)
 
     # Sort the index in the desired order
-    index_order = pd.Index(pd.date_range(start='2000-10-01', end='2001-09-30')).strftime('%m-%d')
-    historic_range_df = historic_range_df.reindex(index_order)
+    #index_order = pd.date_range(start='2023-10-01', end='2024-09-30', freq='D').strftime('%m-%d')
+    #historic_range_df = historic_range_df.reindex(index_order)
 
-    return historic_range_df
+    # Get current year
+    current_year = pd.to_datetime('today').year
+    # If current month is October, November, or December, set the year to next year
+    if pd.to_datetime('today').month not in [10, 11, 12]:
+        # Subtract 1 from current year
+        water_year_start = current_year - 1
+        water_year_end = current_year
+    else:
+        water_year_start = current_year
+        water_year_end = current_year + 1
 
-def plot_annual_hydrograph_statistics(station_number,variable, historic_range_df, realtime_df_daily, save_png=False, png_path=None): 
+    # Convert current year to string
+    water_year_start = str(water_year_start)
+    water_year_end = str(water_year_end)
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))# sharex=True)
+    new_index = []
+    for date in historic_range_df.index:
+        month, day = date.split('-')
+        if month in ['10', '11', '12']:
+            new_date = f"{month}-{day}-{water_year_start}"
+        else:
+            new_date = f"{month}-{day}-{water_year_end}"
+
+        new_index.append(new_date)
+    
+    # Make new_index a DatetimeIndex
+    new_index = pd.to_datetime(new_index)
+
+    # Create new DataFrame with modified index
+    historic_range_df_with_year = pd.DataFrame(historic_range_df.values, index=new_index, columns=historic_range_df.columns)
+    #Sort the index
+    historic_range_df_with_year.sort_index(inplace=True)
+
+    return historic_range_df_with_year
+
+def plot_annual_hydrograph_statistics(station_number,variable, historic_range_df, realtime_df, forecast_df=None, analysis_df=None, threshold_df=None, save_png=False, png_path=None): 
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))
 
     # Plot the main hydrograph
     ax1.fill_between(historic_range_df.index, historic_range_df['Max'], historic_range_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
     ax1.fill_between(historic_range_df.index, historic_range_df['90th'], historic_range_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
     ax1.fill_between(historic_range_df.index, historic_range_df['75th'], historic_range_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
-    ax1.plot(realtime_df_daily.index, realtime_df_daily.values, color='red', label='RealTime')
+    ax1.plot(realtime_df.index, realtime_df.values, color='red', label='Real Time Gauge')
+    if forecast_df is not None:
+        ax1.plot(forecast_df.index, forecast_df.values, color='green', label='NSRPS Forecast')
+    if analysis_df is not None:
+        ax1.scatter(analysis_df.index, analysis_df.values, color='black', label='NSRPS Analysis')
     ax1.set_title(f'{variable} Hydrograph for Complete Water Year, Station {station_number}')
     ax1.set_ylabel(variable)
-    ax1.legend()
-    ax1.set_ylim(0,)
+    #ax1.set_ylim(0,)
     ax1.set_xlim(historic_range_df.index[0], historic_range_df.index[-1])
     ax1.grid(True)
+    
+    #Check if threshold_df is not empty or None
+    if threshold_df is not None:
+        add_thresholds(threshold_df,ax1)
+ 
+    ax1.legend()
 
-    # Format the x-axis tick labels
-    ax1.set_xticks(np.arange(0, len(historic_range_df), step=len(historic_range_df)//12))
-    ax1.set_xticklabels([date[0:5] for date in historic_range_df.index[::len(historic_range_df)//12]], rotation=45)
-
+    #Combine index of realtime_df and forecast_df
+    if forecast_df is not None:
+        combined_index = realtime_df.index.union(forecast_df.index)
+    else:
+        combined_index = realtime_df.index
     #Subset the data to the period of interest (defined by realtime_df_daily.index)
-    historic_range_df = historic_range_df.loc[realtime_df_daily.index]
+    historic_range_df = historic_range_df.loc[combined_index]
     
     # Plot the zoomed-in hydrograph
     ax2.fill_between(historic_range_df.index, historic_range_df['Max'], historic_range_df['Min'], color='lightblue', alpha=0.3, label='Max-Min Range')
     ax2.fill_between(historic_range_df.index, historic_range_df['90th'], historic_range_df['10th'], color='skyblue', alpha=0.3, label='90-10 Percentile Range')
     ax2.fill_between(historic_range_df.index, historic_range_df['75th'], historic_range_df['25th'], color='steelblue', alpha=0.3, label='25-75 Percentile Range')
-    ax2.plot(realtime_df_daily.index, realtime_df_daily.values, color='red', label='RealTime')
+    ax2.plot(realtime_df.index, realtime_df.values, color='red', label='Real Time Gauge')
+    if forecast_df is not None:
+        ax2.plot(forecast_df.index, forecast_df.values, color='green', label='NSRPS Forecast')
+    if analysis_df is not None:
+        ax2.scatter(analysis_df.index, analysis_df.values, color='black', label='NSRPS Analysis')
     ax2.set_title(f'{variable} for Real-Time and Forecasted Data, Station {station_number}')
     ax2.set_xlabel('Day of the Year')
     ax2.set_ylabel(variable)
+
     ax2.legend()
     ax2.grid(True)
 
     # Set the x-axis limits of the zoomed-in hydrograph to match the range of realtime_df_daily.index
-    ax2.set_xlim(realtime_df_daily.index[0], realtime_df_daily.index[-1])
+    ax2.set_xlim(combined_index[0],)
     #Set lower y-axis limit to 0
-    ax2.set_ylim(0,)
-
-    ax2.set_xticks(np.arange(0, len(historic_range_df), step=len(historic_range_df)//12))
-    ax2.set_xticklabels([date[0:5] for date in historic_range_df.index[::len(historic_range_df)//12]], rotation=45)
+    #ax2.set_ylim(0,)
 
     plt.tight_layout()
     plt.show()
 
-    if save_png:
-        plt.savefig(png_path, dpi=300)
-
-def plot_realtime_hydrograph_with_historical_ranges(station_number,variable,historic_df, realtime_df, save_png=False, png_path=None):
+def plot_detailed_hydrograph(station_number,variable, historic_df, realtime_df, forecast_df=None,analysis_df=None, threshold_df=None, save_png=False, png_path=None):
 
     historic_range_df = calculate_daily_percentiles_of_historic_data(historic_df,variable)
 
-    realtime_df_daily = convert_to_daily_mean(realtime_df,variable)
+    realtime_daily_df = convert_to_daily_mean(realtime_df, variable)
 
-    month_day = realtime_df_daily.index.strftime('%m-%d')
-    daily_mean_df = pd.DataFrame(realtime_df_daily.values, index=month_day)
-    
-    plot_annual_hydrograph_statistics(station_number, variable, historic_range_df, daily_mean_df,save_png, png_path)
+    if forecast_df is not None:
+        forecast_df = convert_to_daily_mean(forecast_df,'Discharge',date_col='time')
 
-if __name__ == '__main__':
+    if analysis_df is not None:
+        analysis_df = convert_to_daily_mean(analysis_df,'Discharge',date_col='time')
 
-    df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/historic/05BL022_historic.csv')
-    realtime_df = pd.read_csv('/Users/drc858/GitHub/ForecastFloodImpact/output/realtime/05BL022_realtime.csv')
-    png_path = '/Users/drc858/GitHub/ForecastFloodImpact/output/05BL022_hydrograph.png'
-    variable = 'DISCHARGE'
+    plot_annual_hydrograph_statistics(station_number, variable, historic_range_df, realtime_daily_df,forecast_df,analysis_df, threshold_df, save_png, png_path)
 
-    plot_realtime_hydrograph_with_historical_ranges('05BL022',variable, df, realtime_df, save_png=True, png_path=png_path)
+    return None
---
+++ scripts/nsrps_data_access.py
@@ -0,0 +1,157 @@
+# Description: This script contains functions to retrieve scalar data from the Environment and Climate Change Canada (ECCC) API.
+# data
+import warnings
+import re
+import time
+import configparser
+from datetime import datetime, timedelta
+import xarray as xr 
+import pandas as pd
+import numpy as np
+from shapely.geometry import Point
+
+# web map services 
+from owslib.wms import WebMapService
+from owslib.wcs import WebCoverageService
+from owslib.wcs import Authentication
+from owslib.ogcapi.features import Features
+
+import threading
+
+# Plot the forecast data
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def query_wms_service_for_forecast_times(layer_name, login,iso_format="%Y-%m-%dT%H:%M:%SZ"):
+    """
+    Query the WMS service for the forecast times available for the layer
+    """
+    # first querying the WMS for time metadata
+    wms = WebMapService(f'https://geo.weather.gc.ca/geomet?&SERVICE=WMS&LAYERS={layer_name}',
+                        version='1.3.0',
+                        auth=Authentication(username=login['Username'], password=login['Password']),
+                        timeout=300)
+    first_datetime, last_datetime, datetime_interval = wms[layer_name].dimensions['time']['values'][0].split('/')
+    oldest_fcst, newest_fcast, interval = wms[layer_name].dimensions['reference_time']['values'][0].split('/')
+
+    # convert dates to datetime objects
+    
+    first = datetime.strptime(first_datetime, iso_format)
+    last = datetime.strptime(last_datetime, iso_format)
+
+    # remove anything that isn't a number from the datetime interval (time between forecasts)
+    intvl = int(re.sub(r'\D', '', datetime_interval))
+
+    # create a list of forecast datetimes (we will add these to the requested data)
+    fcasthrs = [first]
+    while first < last:
+        first = first + timedelta(hours=intvl)
+        fcasthrs.append(first)
+
+    return newest_fcast, fcasthrs
+
+def query_wms_service_for_analysis_times(layer_name, login,iso_format="%Y-%m-%dT%H:%M:%SZ"):
+    """
+    Query the WMS service for the forecast times available for the layer
+    """
+    # first querying the WMS for time metadata
+    wms = WebMapService(f'https://geo.weather.gc.ca/geomet?&SERVICE=WMS&LAYERS={layer_name}',
+                        version='1.3.0',
+                        auth=Authentication(username=login['Username'], password=login['Password']),
+                        timeout=300)
+    
+    first_datetime, last_datetime, datetime_interval = wms[layer_name].dimensions['time']['values'][0].split('/')
+    oldest_analysis, newest_analysis, interval = wms[layer_name].dimensions['reference_time']['values'][0].split('/')
+
+    # convert dates to datetime objects
+    
+    first = datetime.strptime(oldest_analysis, iso_format)
+    last = datetime.strptime(newest_analysis, iso_format)
+
+    # remove anything that isn't a number from the datetime interval (time between forecasts)
+    intvl = int(re.sub(r'\D', '', interval))
+
+    # create a list of forecast datetimes (we will add these to the requested data)
+    analysishrs = [first]
+    while first < last:
+        first = first + timedelta(hours=intvl)
+        analysishrs.append(first)
+
+    return first_datetime
+
+def query_wcs_service_for_analysis_data(layer_name, login, newest_analysis, iso_format="%Y-%m-%dT%H:%M:%SZ"):
+    """
+    Query the WCS service for the analysis data
+    """
+    wcs = WebCoverageService(f'https://geo.weather.gc.ca/geomet?&SERVICE=WCS&COVERAGEID={layer_name}', 
+                    auth=Authentication(username=login['Username'], password=login['Password']),
+                    version='2.0.1',
+                    timeout=300)
+
+    response = wcs.getCoverage(identifier=[layer_name], 
+                               format='image/netcdf', 
+                               subsettingcrs='EPSG:4326', 
+                               subsets=[('lat', 50, 52.0), ('lon', -117.0, -113.0)],
+                               DIM_REFERENCE_TIME=newest_analysis,
+                               TIME=newest_analysis)
+    ds = xr.open_dataset(response.read()).load()
+    ds = ds.expand_dims(time=[datetime.strptime(newest_analysis, iso_format)])
+    
+    return ds
+
+def query_wcs_service_for_forecast_data(layer_name, login, newest_fcast, fcasthrs, iso_format="%Y-%m-%dT%H:%M:%SZ"):
+
+    def fetch_data(hr, layer_name, arrys, idx):
+        logger.info(f'Querying {newest_fcast}m lead time {hr}')
+        response = wcs.getCoverage(identifier=[layer_name], 
+                                   format='image/netcdf', 
+                                   subsettingcrs='EPSG:4326', 
+                                   subsets=[('lat', 50, 52.0), ('lon', -117.0, -113.0)],
+                                   DIM_REFERENCE_TIME=newest_fcast, 
+                                   TIME=hr)
+        ds = xr.open_dataset(response.read()).load()
+        ds = ds.expand_dims(time=[fcasthrs[idx] + timedelta(hours=time_zone)])
+        arrys[idx] = ds
+
+    fcasthrs_str = [datetime.strftime(hr, iso_format) for hr in fcasthrs]
+
+    wcs = WebCoverageService(f'https://geo.weather.gc.ca/geomet?&SERVICE=WCS&COVERAGEID={layer_name}', 
+                    auth=Authentication(username=login['Username'], password=login['Password']),
+                    version='2.0.1',
+                    timeout=300)
+
+    time_zone = -7
+
+    arrys = [None] * len(fcasthrs_str)
+    threads = []
+
+    for i, hr in enumerate(fcasthrs_str):
+        thread = threading.Thread(target=fetch_data, args=(hr, layer_name, arrys, i))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    fcasts = xr.concat([ds for ds in arrys if ds is not None], dim='time')
+    
+    return fcasts
+    
+
+def extract_station_from_grid(station, input_ds):
+    """
+    Extract from the grided data for a given station
+    """
+
+    # Extract the station location
+    station_location = Point(station[1]['MODEL_LONGITUDE'], station[1]['MODEL_LATITUDE'])
+
+    # Extract the station data
+    station_data = input_ds.sel(lon=station_location.x, lat=station_location.y, method='nearest')
+
+    return station_data
